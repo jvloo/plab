60199d77ba62ad88705ae71029d01f756bd1334a
Xavier Loo <xavier.jh130@gmail.com>
2018-12-24 23:07:53 +0800

    Delete log-2018-12-24.php

--- core/plab/logs/log-2018-12-24.php
+++
@@ -1,3 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-
-ERROR - 2018-12-24 20:50:57 --> $config['composer_autoload'] is set to TRUE but D:\server\www\plab\core\plab\vendor/autoload.php was not found.
